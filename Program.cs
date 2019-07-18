1c94f9c828adf777584c462aa6d161e02bc51643
Hans Roos <hans.roos@entelect.co.za>
2019-07-18 14:27:45 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace monte_pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
